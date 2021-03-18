7865c1ed4eeb9dc7ae2137799e06b886deaf6104
beccasouza <rebeccacsouza@gmail.com>
2021-03-18 16:57:04 -0500

    First commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,19 @@ import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyCL1mh7NIp8iNfow1PgSPBkD76Om_XrhB0',
+  authDomain: 'team-antheia-capstone.firebaseapp.com',
+  projectId: 'team-antheia-capstone',
+  storageBucket: 'team-antheia-capstone.appspot.com',
+  messagingSenderId: '790939626127',
+  appId: '1:790939626127:web:45df9ee2fd8b0732c75efa',
+  measurementId: 'G-NF36NH4Y6Y',
+};
+
+firebase.initializeApp(firebaseConfig);
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
