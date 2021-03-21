561b4a35cd0be4d45bed6db7b263ed00060f8bcc
Siray Wali <school@Sirays-Mac-mini.local>
2021-03-21 01:15:58 -0500

    Feat(Demo): adds demo scrapbook view with Modal edit view

--- src/app/features/DemoScrapbook.js
+++ src/app/features/DemoScrapbook.js
@@ -1,46 +1,76 @@
-import React from "react";
+import React, { Component } from "react";
 import FlipPage from "react-flip-page";
+import Page1 from "./demo/DemoPage1";
+import { Button, Modal } from "rsuite";
 
-export default function DemoScrapbook() {
-  return (
-    <div style={styles.container}>
-      <FlipPage width={800} height={525} orientation="horizontal">
-        <div style={styles.twoPage}>
-          <div style={styles.singlePage}>
-            <article>
-              <h1>My awesome first article</h1>
-              <p>My awesome first content</p>
-            </article>
-          </div>
-          <div style={styles.singlePage}>
-            <article>
-              <h1>My wonderful second article</h1>
-              <p>My wonderful second content</p>
-            </article>
-          </div>
-        </div>
+export default class DemoScrapbook extends Component {
+  constructor() {
+    super();
 
-        <div style={styles.twoPage}>
-          <div style={styles.singlePage}>
-            <article>
-              <h1>My awesome third article</h1>
-              <p>My awesome third content</p>
-            </article>
+    this.state = {
+      show: false,
+    };
+    this.open = this.open.bind(this);
+    this.close = this.close.bind(this);
+  }
+  open() {
+    this.setState({ show: true });
+  }
+  close() {
+    this.setState({ show: false });
+  }
+
+  render() {
+    return (
+      <div style={styles.container}>
+        <FlipPage width={800} height={525} orientation="horizontal">
+          <div style={styles.twoPage}>
+            <div style={styles.singlePage}>
+              <article>
+                <Page1 isStatic={true} />
+                <Button onClick={this.open}>edit page</Button>
+              </article>
+            </div>
+            <div style={styles.singlePage}>
+              <article>
+                <h1>My wonderful second article</h1>
+                <p>My wonderful second content</p>
+              </article>
+            </div>
           </div>
-          <div style={styles.singlePage}>
-            <article>
-              <h1>My wonderful fourth article</h1>
-              <p>My wonderful fourth content</p>
-            </article>
+
+          <div style={styles.twoPage}>
+            <div style={styles.singlePage}>
+              <article>
+                <h1>My awesome third article</h1>
+                <p>My awesome third content</p>
+              </article>
+            </div>
+            <div style={styles.singlePage}>
+              <article>
+                <h1>My wonderful fourth article</h1>
+                <p>My wonderful fourth content</p>
+              </article>
+            </div>
           </div>
-        </div>
-      </FlipPage>
-    </div>
-  );
+        </FlipPage>
+        <Modal overflow={true} backdrop={true} show={this.state.show}>
+          <Page1 />
+          <Button onClick={this.close}>Close</Button>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 const styles = {
   twoPage: { display: "flex", justifyContent: "space-around", padding: "auto" },
-  container: { padding: 8, backgroundColor: "red" },
+  container: {
+    padding: 8,
+    backgroundColor: "red",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   singlePage: { width: 390, backgroundColor: "green", minHeight: 500 },
 };
--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app';
-import store from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+import store from "./app/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+import "rsuite/dist/styles/rsuite-default.css";
 
-import firebase from 'firebase/app';
-import 'firebase/storage';
-
-
-
-
+import firebase from "firebase/app";
+import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCL1mh7NIp8iNfow1PgSPBkD76Om_XrhB0',
-  authDomain: 'team-antheia-capstone.firebaseapp.com',
-  projectId: 'team-antheia-capstone',
-  storageBucket: 'team-antheia-capstone.appspot.com',
-  messagingSenderId: '790939626127',
-  appId: '1:790939626127:web:45df9ee2fd8b0732c75efa',
-  measurementId: 'G-NF36NH4Y6Y',
+  apiKey: "AIzaSyCL1mh7NIp8iNfow1PgSPBkD76Om_XrhB0",
+  authDomain: "team-antheia-capstone.firebaseapp.com",
+  projectId: "team-antheia-capstone",
+  storageBucket: "team-antheia-capstone.appspot.com",
+  messagingSenderId: "790939626127",
+  appId: "1:790939626127:web:45df9ee2fd8b0732c75efa",
+  measurementId: "G-NF36NH4Y6Y",
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -32,7 +29,7 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
