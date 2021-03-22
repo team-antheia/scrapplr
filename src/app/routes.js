de5a7a0e3f7064a3cb41ed1c57814d8a4206a58f
beccasouza <rebeccacsouza@gmail.com>
2021-03-22 10:24:05 -0500

    fix(routes/index): updates history prop on router

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,39 +1,26 @@
-
-import React, { Component } from "react";
-import { LandingPage } from "./components";
-import Map from "./components/GoogleAPIWrapper";
-import { Route } from "react-router-dom";
-
-
 import React, { Component } from 'react';
 
-import { Router, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { LandingPage } from './components';
 import { Login } from './components/Login';
 import { SignUp } from './components/Signup';
-import history from './history';
 import Map from './components/GoogleAPIWrapper';
-import { DemoScrapbook } from "./features";
-
-
+import { DemoScrapbook } from './features';
 
 export default class routes extends Component {
   render() {
     return (
-
       <div>
         <Route exact path="/">
           <LandingPage />
         </Route>
-      <Route exact path="/demo">
+        <Route exact path="/demo">
           <DemoScrapbook />
-      <Map />
+          <Map />
         </Route>
-      <Route path="/signup" component={SignUp} />
+        <Route path="/signup" component={SignUp} />
         <Route path="/login" component={Login} />
-        
       </div>
-
     );
   }
 }
