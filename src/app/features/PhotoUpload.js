a2dc937f4cf546fa5d45eed7072104f2143c2cc0
beccasouza <rebeccacsouza@gmail.com>
2021-03-24 10:14:47 -0500

    chore: merged with master

--- src/app/features/PhotoUpload.js
+++ src/app/features/PhotoUpload.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { storage } from '../../index';
+import firebase, { storage, firestore, auth } from '../../index';
 import { EXIF } from 'exif-js';
 
 function PhotoUpload() {
   //declares names in state, set to empty string
-  const [imageAsFile, setImageAsFile] = useState(null);
+  const [imageAsFile, setImageAsFile] = useState('');
   const [imageAsUrl, setImageAsUrl] = useState('');
+  const [latitude, setLat] = useState('');
+  const [longitude, setLon] = useState('');
 
   //When photo is uploaded, grab data and set to state
   const handleImageAsFile = (e) => {
@@ -18,6 +20,48 @@ function PhotoUpload() {
   };
 
   const handleFirebaseUpload = (e) => {
+    function ConvertDMSToDD(degrees, minutes, seconds, direction) {
+      var dd = degrees + minutes / 60 + seconds / 3600;
+
+      if (direction === 'S' || direction === 'W') {
+        dd = dd * -1;
+      }
+
+      return dd;
+    }
+
+    EXIF.getData(imageAsFile, function () {
+      // Calculate latitude decimal
+      var latDegree = imageAsFile.exifdata.GPSLatitude[0].numerator;
+      var latMinute = imageAsFile.exifdata.GPSLatitude[1].numerator;
+      var latSecond = imageAsFile.exifdata.GPSLatitude[2].numerator;
+      var latDirection = imageAsFile.exifdata.GPSLatitudeRef;
+
+      var latFinal = ConvertDMSToDD(
+        latDegree,
+        latMinute,
+        latSecond,
+        latDirection
+      );
+      setLat(latFinal);
+      console.log(latFinal);
+
+      // Calculate longitude decimal
+      var lonDegree = imageAsFile.exifdata.GPSLongitude[0].numerator;
+      var lonMinute = imageAsFile.exifdata.GPSLongitude[1].numerator;
+      var lonSecond = imageAsFile.exifdata.GPSLongitude[2].numerator;
+      var lonDirection = imageAsFile.exifdata.GPSLongitudeRef;
+
+      var lonFinal = ConvertDMSToDD(
+        lonDegree,
+        lonMinute,
+        lonSecond,
+        lonDirection
+      );
+      setLon(lonFinal);
+      console.log(lonFinal);
+    });
+
     e.preventDefault();
     //upload file to firebase storage
     const uploadTask = storage
@@ -51,6 +95,41 @@ function PhotoUpload() {
     );
   };
 
+  //separate into two functions;
+  //find function for place I want to change
+  //update function that takes in that place and makes update
+  async function updateDatabase() {
+    let userId = auth.currentUser.uid;
+
+    let snapshot = await firestore
+      .collection('Scrapbooks')
+      .where('Owner', '==', userId)
+      .get();
+    //   // .collection('Map Points')
+    if (snapshot.empty) {
+      console.log('No matching documents.');
+    }
+
+    snapshot.forEach((scrapbook) => {
+      // console.log('scrapbook', '=>', scrapbook.data().Owner);
+      console.log(scrapbook.data());
+      // if (scrapbook.data().Owner === userId) {
+      //   console.log(scrapbook.data()['Map Points']);
+      // }
+    });
+
+    // try {
+    //   await firestore.runTransaction(async (t) => {
+    //     const doc = await t.get(scrapbookRef)
+
+    //     doc.data().
+    //   })
+    //   console.log('Success!')
+    // } catch (e) {
+    //   console.log('Failed', e)
+    // }
+  }
+
   return (
     <div className="photo-upload">
       <h1>Upload a photo</h1>
@@ -62,7 +141,10 @@ function PhotoUpload() {
         />
         <button>Upload</button>
       </form>
-      <img src={imageAsUrl} alt="" />
+      {/* <img src={imageAsUrl} alt="" /> */}
+      <button type="button" onClick={updateDatabase}>
+        Update DB
+      </button>
     </div>
   );
 }
