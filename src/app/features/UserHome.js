a70878cb2e40634db90ef17b00a3689b0b731538
Marjorie Mejia <mm035359@gmail.com>
2021-03-26 15:12:08 -0400

    feature(): modified:   package-lock.json

--- src/app/features/UserHome.js
+++ src/app/features/UserHome.js
@@ -1,4 +1,6 @@
-import firebase, { auth, firestore } from "../../index";
+import { auth, firestore } from '../../index';
+import firebase from 'firebase/app';
+import 'firebase/auth'; // 👈 this could also be in your `firebase.js` file
 import {
   Heading,
   Button,
@@ -10,12 +12,12 @@ import {
   CardFooter,
   Image,
   Text,
-} from "grommet";
-import { Link } from "react-router-dom";
-import BookCard from "../components/BookCard";
-import history from "../history";
+} from 'grommet';
+import { Link } from 'react-router-dom';
+import BookCard from '../components/BookCard';
+import history from '../history';
 
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export default class UserHome extends Component {
   constructor() {
@@ -26,20 +28,33 @@ export default class UserHome extends Component {
       scrapbooks: [],
     };
     this.handleLogout = this.handleLogout.bind(this);
+    this.getScrapbooks = this.getScrapbooks.bind(this);
   }
 
   async componentDidMount() {
-    const user = firebase.auth().currentUser;
-    const userId = user.uid;
-    const scrapbooks = await this.getScrapbooks(userId);
+    //CURRENT USER IS NULL
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log('in the new function');
+      if (user) {
+        console.log('user in userhome', user);
+        // User is signed in.
+        this.setState({ user: user });
+        const userId = user.uid;
+
+        const scrapbooks = this.getScrapbooks(userId);
+      } else {
+        // No user is signed in.
+        console.log('Not logged in');
+      }
+    });
   }
 
   async getScrapbooks(userId) {
-    const scrapbooksRef = firestore.collection("Scrapbooks");
-    const queryRef = await scrapbooksRef.where("owner", "==", userId).get();
-    console.log("userHome userId", userId);
+    const scrapbooksRef = firestore.collection('Scrapbooks');
+    const queryRef = await scrapbooksRef.where('owner', '==', userId).get();
+    console.log('userHome userId', userId);
     if (queryRef.empty) {
-      console.log("no matching documents");
+      console.log('no matching documents');
       return;
     }
     queryRef.forEach((doc) => {
@@ -52,12 +67,12 @@ export default class UserHome extends Component {
 
   async handleLogout() {
     await firebase.auth().signOut();
-    history.push("/login");
+    history.push('/login');
   }
 
   render() {
-    console.log("userHome props", this.props);
-    const { history, user } = this.props;
+    // console.log('userHome props', this.props);
+    // const { history, user } = this.props;
 
     // map over scrapbooks and grab the following information:
 
@@ -65,23 +80,23 @@ export default class UserHome extends Component {
     // scrapbook title
     // scapnook owner
     // scrapbook coverphoto
-    console.log("userhome state", this.state.scrapbooks);
+    //console.log('userhome state', this.props);
 
     return (
       <ResponsiveContext.Consumer>
         {(size) => (
           <Box
-            align="center"
-            height="85vh"
-            width={size === "small" ? "80vw" : "75vw"}
-            direction="column"
+            align='center'
+            height='85vh'
+            width={size === 'small' ? '80vw' : '75vw'}
+            direction='column'
           >
-            <Heading level={3}>welcome {user.email}</Heading>
+            <Heading level={3}>welcome {this.state.user.email}</Heading>
             {this.state.scrapbooks.map((book) => {
-              console.log("mapping");
-              return <BookCard {...book} email={user.email} />;
+              console.log('mapping');
+              return <BookCard {...book} email={this.state.user.email} />;
             })}
-            <Button label="logout" onClick={this.handleLogout} />
+            <Button label='logout' onClick={this.handleLogout} />
           </Box>
         )}
       </ResponsiveContext.Consumer>
--- src/app/routes.js
+++ src/app/routes.js
@@ -1,51 +1,41 @@
 import React, { Component } from 'react';
 
-
-import { Route, Switch } from "react-router-dom";
-import { LandingPage } from "./components";
-import { Login } from "./components/Login";
-import { SignUp } from "./components/Signup";
-import { DemoScrapbook, BookShelfView, UserHome } from "./features";
-import { Box } from "grommet";
-import { auth } from "../index";
-import LocationSearchInput from './components/GoogleSuggest'
+import { Route, Switch } from 'react-router-dom';
+import { LandingPage } from './components';
+import { Login } from './components/Login';
+import { SignUp } from './components/Signup';
+import { DemoScrapbook, BookShelfView, UserHome } from './features';
+import { Box } from 'grommet';
+import { auth } from '../index';
+import LocationSearchInput from './components/GoogleSuggest';
 import { StreetView } from './components/StreetView';
 import MapContainer from './components/MapContainer';
 
-
 export default class routes extends Component {
   render() {
-    const isLoggedIn = !!auth.currentUser;
-    console.log("user in routes", auth.currentUser);
+    // const isLoggedIn = !!auth.currentUser;
+    // console.log('user in routes', auth.currentUser);
 
     return (
-
-      <Box justify="center" align="center" height="100vh">
-        <Route
-          exact
-          path="/home"
-          component={() => (
-            <UserHome user={auth.currentUser ? auth.currentUser : false} />
-          )}
-        />
+      <Box justify='center' align='center' height='100vh'>
+        <Route exact path='/home' component={() => <UserHome />} />
 
         <Switch>
-          <Route exact path="/">
+          <Route exact path='/'>
             <LandingPage />
           </Route>
-          <Route exact path="/demo">
+          <Route exact path='/demo'>
             <DemoScrapbook />
           </Route>
-          <Route path="/signup" component={SignUp} />
-          <Route path="/login" component={Login} />
-          <Route path="/bookshelf">
+          <Route path='/signup' component={SignUp} />
+          <Route path='/login' component={Login} />
+          <Route path='/bookshelf'>
             <BookShelfView />
           </Route>
-        <Route path="/test" component={LocationSearchInput} />
-        <Route path='/streetview' component={StreetView} />
+          <Route path='/test' component={LocationSearchInput} />
+          <Route path='/streetview' component={StreetView} />
         </Switch>
       </Box>
     );
   }
 }
-
