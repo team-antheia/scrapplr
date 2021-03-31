6b44bebe6d97d99641970050bce31e9439f8a659
Siray Wali <school@Sirays-Mac-mini.local>
2021-03-31 05:56:31 -0500

    Feat(scrapbook): adds template grids

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,23 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { Route, Switch } from 'react-router-dom';
-import { LandingPage } from './components';
-import { Login } from './components/auth/Login';
-import { SignUp } from './components/auth/Signup';
-import { ScrapbookView, BookShelfView, UserHome } from './components';
-import { Box } from 'grommet';
-import firebase, { auth, firestore } from '../index';
-import SinglePage from './components/scrapbook/SinglePage';
-import LocationSearchInput from './components/map/360/LocationSearchInput';
-import StreetView from './components/map/360/StreetView';
-import MapContainer from './components/map/markerMap/MapContainer';
+import { Route, Switch } from "react-router-dom";
+import { LandingPage } from "./components";
+import { Login } from "./components/auth/Login";
+import { SignUp } from "./components/auth/Signup";
+import { ScrapbookView, BookShelfView, UserHome } from "./components";
+import { Box, Heading } from "grommet";
+import firebase, { auth, firestore } from "../index";
+import SinglePage from "./components/scrapbook/SinglePage";
+import LocationSearchInput from "./components/map/360/LocationSearchInput";
+import StreetView from "./components/map/360/StreetView";
+import MapContainer from "./components/map/markerMap/MapContainer";
+import Default from "./components/scrapbook/layouts/Default";
+import CaptionTop from "./components/scrapbook/layouts/CaptionTop";
+import CaptionBottom from "./components/scrapbook/layouts/CaptionBottom";
+import CaptionMiddle from "./components/scrapbook/layouts/CaptionMiddle";
 
 export default class routes extends Component {
   constructor() {
     super();
     this.state = {
-      userId: '',
-      email: '',
+      userId: "",
+      email: "",
     };
   }
   async componentDidMount() {
@@ -25,12 +29,12 @@ export default class routes extends Component {
       if (auth) {
         const email = auth.email;
         // User is signed in. Uses email to find uid from db
-        const usersRef = firestore.collection('Users');
-        const user = await usersRef.where('email', 'in', [email]).get();
+        const usersRef = firestore.collection("Users");
+        const user = await usersRef.where("email", "in", [email]).get();
         if (!user) {
-          console.log('No user found');
+          console.log("No user found");
         } else {
-          console.log('User found');
+          console.log("User found");
           this.setState({
             userId: user.docs[0].id,
             email: email,
@@ -38,14 +42,14 @@ export default class routes extends Component {
         }
       } else {
         // No user is signed in.
-        console.log('Not logged in');
+        console.log("Not logged in");
       }
     });
   }
 
   render() {
     return (
-      <Box justify='center' align='center' height='100vh'>
+      <Box justify="center" align="center" height="100vh">
         {/* {auth.currentUser !== null && auth.currentUser.email && (
           <Route
             exact
@@ -60,12 +64,12 @@ export default class routes extends Component {
         )} */}
 
         <Switch>
-          <Route exact path='/'>
+          <Route exact path="/">
             <LandingPage />
           </Route>
 
           <Route
-            path='/scrapbooks/:scrapbookId'
+            path="/scrapbooks/:scrapbookId"
             render={(props) => (
               <ScrapbookView
                 userId={this.state.userId}
@@ -73,21 +77,21 @@ export default class routes extends Component {
               />
             )}
           ></Route>
-          <Route path='/demo'>
-            <ScrapbookView user={'demo'} />
+          <Route path="/demo">
+            <ScrapbookView user={"demo"} />
           </Route>
-          <Route path='/signup' component={SignUp} />
-          <Route path='/login' component={Login} />
-          <Route path='/bookshelf'>
+          <Route path="/signup" component={SignUp} />
+          <Route path="/login" component={Login} />
+          <Route path="/bookshelf">
             <BookShelfView />
           </Route>
-          <Route path='/test' component={LocationSearchInput} />
-          <Route path='/streetview' component={StreetView} />
+          <Route path="/test" component={LocationSearchInput} />
+          <Route path="/streetview" component={StreetView} />
           {/* <Route path="/map" component={MapContainer} /> */}
           {auth.currentUser !== null && auth.currentUser.email && (
             <Route
               exact
-              path='/home'
+              path="/home"
               component={() => (
                 <UserHome
                   userId={auth.currentUser ? this.state.userId : false}
@@ -96,7 +100,27 @@ export default class routes extends Component {
               )}
             />
           )}
-          <Route path='/' component={LandingPage} />
+          <Route path="/grids">
+            <Box width="100%" pad="medium">
+              <Heading margin="small" level="4">
+                Default grid 👇🏽
+              </Heading>
+              <Default />
+              <Heading margin="medium" level="4">
+                Caption Top 👇🏽
+              </Heading>
+              <CaptionTop />
+              <Heading margin="medium" level="4">
+                Caption Middle 👇🏽
+              </Heading>
+              <CaptionMiddle />
+              <Heading margin="medium" level="4">
+                Caption Bottom 👇🏽
+              </Heading>
+              <CaptionBottom />
+            </Box>
+          </Route>
+          <Route path="/" component={LandingPage} />
         </Switch>
       </Box>
     );
