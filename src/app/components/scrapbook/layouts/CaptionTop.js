c4b50ee068453e9e26379936b3ff3f2d2c82dd45
beccasouza <rebeccacsouza@gmail.com>
2021-04-01 16:12:03 -0500

    fix/chore: refactors ScrapbookView to react hooks; fixes bug  so cards load from firestore

--- src/app/components/scrapbook/layouts/CaptionTop.js
+++ src/app/components/scrapbook/layouts/CaptionTop.js
@@ -1,20 +1,20 @@
-import React from "react";
-import { Grid, Card, Text, Image, Anchor } from "grommet";
-import StreetView from "../../map/360/StreetView"
+import React from 'react';
+import { Grid, Card, Text, Image, Anchor } from 'grommet';
+import StreetView from '../../map/360/StreetView';
 
 export default function CaptionTop(props) {
-  console.log("the props ", props);
+  console.log('the props ', props);
 
   const cards = props.page.cards;
   return (
     <Grid
-      rows={["flex", "flex", "flex"]}
-      columns={["flex", "flex"]}
+      rows={['flex', 'flex', 'flex']}
+      columns={['flex', 'flex']}
       gap="xsmall"
       areas={[
-        { name: "caption", start: [0, 0], end: [0, 0] },
-        { name: "media", start: [0, 1], end: [0, 1] },
-        {name:'media2' , start:[0,2], end:[1,2]}
+        { name: 'caption', start: [0, 0], end: [0, 0] },
+        { name: 'media', start: [0, 1], end: [0, 1] },
+        { name: 'media2', start: [0, 2], end: [1, 2] },
       ]}
     >
       <div>
@@ -25,24 +25,22 @@ export default function CaptionTop(props) {
           return (
             <div>
               <div>
-                {card.type === "description" && (
+                {card.type === 'description' && (
                   <Card gridArea="caption" background="brand">
                     <Text>{card.body}</Text>
                   </Card>
                 )}
               </div>
-              {card.type === "panoramic" && (
-                  <Card gridArea="media" background="brand" flex>
-                  <StreetView lat={card.body._lat} long={card.body._long}/></Card>
-                )}
+              {card.type === 'panoramic' && (
+                <Card gridArea="media" background="brand" flex>
+                  <StreetView lat={card.body._lat} long={card.body._long} />
+                </Card>
+              )}
               <div>
-                {card.type === "image" && (
+                {card.type === 'image' && (
                   <Card gridArea="media2" background="brand">
                     <Anchor href="#">
-                      <Image
-                        fill
-                        src="https://media.cntraveler.com/photos/53fc86a8a5a7650f3959d273/master/pass/travel-with-polaroid-camera.jpg"
-                      />
+                      <Image fill src={card.body} />
                     </Anchor>
                   </Card>
                 )}
