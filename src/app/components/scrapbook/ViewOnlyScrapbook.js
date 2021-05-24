2e0268f5f62877dea6424562fc3844655e60b678
Marjorie Mejia <mm035359@gmail.com>
2021-05-24 16:43:48 -0400

    fe: updated readme

--- src/app/components/scrapbook/ViewOnlyScrapbook.js
+++ src/app/components/scrapbook/ViewOnlyScrapbook.js
@@ -1,9 +1,5 @@
 import React, { useEffect, useState } from 'react';
-<<<<<<< HEAD
-//import FlipPage from 'react-flip-page';
-=======
 import { withRouter } from 'react-router-dom';
->>>>>>> master
 
 import {
   Box,
@@ -72,14 +68,14 @@ function ScrapbookView(props) {
 
   return pages.length ? (
     <Box>
-      <Box margin={{ bottom: 'medium' }} direction="row" max="500px">
+      <Box margin={{ bottom: 'medium' }} direction='row' max='500px'>
         <Button
-          type="button"
-          className="backHome"
-          label="sign up with scrapplr"
+          type='button'
+          className='backHome'
+          label='sign up with scrapplr'
           onClick={backHome}
           primary
-          margin="small"
+          margin='small'
           style={{ height: '100%' }}
         />
       </Box>
@@ -88,138 +84,14 @@ function ScrapbookView(props) {
         {props.location.state.title}
       </Heading> */}
       <Box
-<<<<<<< HEAD
-        width={{ min: '85vw' }}
-        height={{ min: '75vh' }}
         justify='center'
         align='center'
-        background={{
-          color: 'neutral-1',
-          opacity: true,
-          position: 'bottom',
-          repeat: 'no-repeat',
-          size: 'cover',
-        }}
-        border={{
-          color: 'border',
-          size: 'large',
-          style: 'groove',
-          side: 'all',
-        }}
-      >
-        <ResponsiveContext.Consumer>
-          {/* mobile view */}
-          {(size) =>
-            size === 'small' ? (
-              <FlipPage
-                disableSwipe={true}
-                flipOnTouch={true}
-                responsive={true}
-                style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  padding: '24x',
-                }}
-              >
-                <Grid
-                  rows={['small', 'small', 'small']}
-                  columns={['small', 'small']}
-                  gap='xsmall'
-                  areas={[
-                    { name: 'card1', start: [0, 0], end: [1, 0] },
-                    { name: 'nav', start: [0, 1], end: [0, 1] },
-                    { name: 'main', start: [1, 1], end: [1, 1] },
-                    { name: 'sub', start: [0, 2], end: [1, 2] },
-                  ]}
-                >
-                  <Card gridArea='card1' background='brand' />
-                  <Card gridArea='nav' background='light-5' />
-                  <Card gridArea='main' background='light-2' />
-                  <Card gridArea='sub' background='light-2' />
-                </Grid>
-                <Grid
-                  rows={['small', 'small', 'small']}
-                  columns={['small', 'small']}
-                  gap='xsmall'
-                  areas={[
-                    { name: 'card1', start: [0, 0], end: [1, 0] },
-                    { name: 'nav', start: [0, 1], end: [0, 1] },
-                    { name: 'main', start: [1, 1], end: [1, 1] },
-                    { name: 'sub', start: [0, 2], end: [1, 2] },
-                  ]}
-                >
-                  <Card gridArea='card1' background='brand' />
-                  <Card gridArea='nav' background='light-5' />
-                  <Card gridArea='main' background='light-2' />
-                  <Card gridArea='sub' background='light-2' />
-                </Grid>
-              </FlipPage>
-            ) : (
-              // Webpage
-              <div
-                style={{
-                  display: 'flex',
-                  width: '100%',
-                  height: '100%',
-                }}
-              >
-                <FlipPage
-                  disableSwipe={true}
-                  height={320}
-                  responsive={true}
-                  orientation='horizontal'
-                >
-                  {pages.length >= 1 ? (
-                    pages.map((page) => {
-                      return (
-                        <div>
-                          {/* <CaptionTop page={pages[pageNum - 1]} /> */}
-
-                          <Text>{page.pageNum}</Text>
-                          <Card background='brand' gridArea='header'>
-                            {page.pageTitle}
-                          </Card>
-                          <CaptionTop cards={cards} />
-                        </div>
-                      );
-                    })
-                  ) : (
-                    <div>
-                      <Box pad='xxsmall'>
-                        <Default />
-                      </Box>
-                      <Box>{/* <CaptionMiddle /> */}</Box>
-                      <Box>{/* <CaptionTop /> */}</Box>
-                      <Box>
-                        <CaptionBottom />
-                      </Box>
-                    </div>
-                  )}
-                  <div></div>
-                  <div></div>
-                </FlipPage>
-              </div>
-            )
-          }
-        </ResponsiveContext.Consumer>
-      </Box>
-      <Button
-        type='button'
-        label='join scrapplr'
-        onClick={backToSignUp}
-        primary
-        margin='small'
-      />
-=======
-        justify="center"
-        align="center"
-        height="large"
-        width="90vw"
+        height='large'
+        width='90vw'
         style={{ maxWidth: '864px' }}
-        background="glass2"
+        background='glass2'
         round={true}
-        border="7px solid black"
+        border='7px solid black'
       >
         <ResponsiveContext.Consumer>
           {(size) => (
@@ -240,7 +112,6 @@ function ScrapbookView(props) {
           )}
         </ResponsiveContext.Consumer>
       </Box>
->>>>>>> master
     </Box>
   ) : (
     <Spinner />
